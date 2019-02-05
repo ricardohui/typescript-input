c50f59096be8d57538df0ac339338133c53b885b
Ricardo <eehaha@gmail.com>
2019-02-05 13:31:50 +0800

    the same test using shallow

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,3 +7,15 @@ it("can renders without crashing using enzyme", () => {
 
   expect(wrapper).toBeDefined();
 });
+
+it("should contain an input", () => {
+  const wrapper = shallow(<App />);
+  expect(wrapper.find("input").length).toEqual(1);
+});
+
+it("has a value of 'text' when onChange", () => {
+  const wrapper = shallow(<App />);
+  wrapper.find("input").simulate("change", { target: { value: "text" } });
+  wrapper.update();
+  expect(wrapper.find("input").prop("value")).toEqual("text");
+});
